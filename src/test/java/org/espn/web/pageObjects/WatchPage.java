81978055d62515142da02e315f7c6b55ad604394
john.arango <johnj42@gmail.com>
2022-12-08 10:05:47 -0500

    WatchPage Added

---
+++ src/test/java/org/espn/web/pageObjects/WatchPage.java
@@ -0,0 +1,82 @@
+package org.espn.web.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class WatchPage extends HomePage{
+
+    public WatchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(className = "watch")
+    private WebElement watchLink;
+
+    @FindBy(css = ".BucketsContainer > div")
+    private List<WebElement> carrouselContainer;
+
+    @FindBy(css = ".BucketsContainer > div:nth-child(1) > section > div:nth-child(2) > div > div > ul > li")
+    private List<WebElement> carrouselCards;
+
+    @FindBy(css = ".BucketsContainer > div:nth-child(1) > section > div:nth-child(2) > div > div > ul > li:nth-child(2)")
+    private WebElement secondCard;
+
+    @FindBy(css = "button.lightbox__closebtn")
+    private WebElement closeButton;
+
+    @FindBy(css = ".user")
+    private WebElement userOnline;
+
+    @FindBy(css = ".display-user > span")
+    private WebElement userName;
+
+    public void navigateToWatch(){
+        clickElement(watchLink);
+    }
+
+    public int getCarrouselContainerSize() {
+        return carrouselContainer.size();
+    }
+
+    public int getNoTitleCards() {
+        int count = 0;
+        for(WebElement carrouselCard : carrouselCards){
+            if(carrouselCard.getText().isEmpty()){
+                count++;
+            }
+        } return count;
+    }
+
+    public void clickSecondCard() {
+        clickElement(secondCard);
+    }
+
+    public boolean validateCloseButton() {
+        waitForClickable(closeButton);
+        return closeButton.isDisplayed();
+    }
+
+    public void waitForCloseBtn(){
+        waitForClickable(closeButton);
+    }
+
+    public void clickCloseButton() {
+        clickElement(closeButton);
+    }
+
+    public void clickBack(){
+        super.getDriver().navigate().back();
+    }
+
+    public WebElement getUserOnline() {
+        return userOnline;
+    }
+
+    public String getUserName() {
+        return userName.getText();
+    }
+
+}
