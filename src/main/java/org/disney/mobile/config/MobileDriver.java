ced33e0940aa2c5f165d95f94ea925c5409ca7b1
john.arango <johnj42@gmail.com>
2022-12-09 15:03:14 -0500

    First Mobile Configuration

--- src/main/java/org/disney/mobile/config/MobileDriver.java
+++ src/main/java/org/disney/mobile/config/MobileDriver.java
@@ -3,15 +3,15 @@ package org.disney.mobile.config;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class Driver {
+public class MobileDriver {
 
     private final AndroidDriver<AndroidElement> driver;
 
-    public Driver(AndroidDriver<AndroidElement> driver) {
+    public MobileDriver(AndroidDriver<AndroidElement> driver) {
         this.driver = driver;
     }
 
     public AndroidDriver<AndroidElement> getDriver() {
-        return driver;
+        return this.driver;
     }
 }
---
+++ src/test/java/org/disney/mobile/tests/steps/MobileHooks.java
@@ -0,0 +1,36 @@
+package org.disney.mobile.tests.steps;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.cucumber.java.Before;
+import org.disney.mobile.config.MobileDriver;
+import org.disney.mobile.util.ConfigCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MobileHooks {
+
+    public static MobileDriver driver;
+
+   @Before
+    public void environmentSetup(){
+       DesiredCapabilities capabilities = new DesiredCapabilities();
+       ConfigCapabilities.deviceSetUp(capabilities);
+       ConfigCapabilities.applicationSetUp(capabilities);
+       try {
+           driver = new MobileDriver(new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),
+                   capabilities));
+       } catch (MalformedURLException exception) {
+           exception.printStackTrace();
+       }
+   }
+
+   /*@After
+    public void tearDown(){driver.getDriver().quit();}*/
+
+    public static AndroidDriver<AndroidElement> getDriver() {
+        return driver.getDriver();
+    }
+}
