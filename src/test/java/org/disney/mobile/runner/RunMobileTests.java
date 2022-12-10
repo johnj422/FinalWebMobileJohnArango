f85772e1b585c453b04d8f7124ac42f80afa435f
john.arango <johnj42@gmail.com>
2022-12-09 22:28:09 -0500

    RunMobileTests JavaDocs

--- src/test/java/org/disney/mobile/runner/RunMobileTests.java
+++ src/test/java/org/disney/mobile/runner/RunMobileTests.java
@@ -3,6 +3,9 @@ package org.disney.mobile.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+/**
+ * Runs the tests defined in MobileSteps
+ */
 @CucumberOptions(
         features = "src/test/java/org/disney/mobile/tests/features",
         glue = "org/disney/mobile/tests/steps"
